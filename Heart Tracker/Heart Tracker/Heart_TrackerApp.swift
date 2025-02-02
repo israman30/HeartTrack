d1f323b661f4124b200e6e24e97b6ddadf6a13cd
Israel Manzo <israelmanzo814@gmail.com>
2025-02-01 21:51:17 -0500

    added new project for HeartTrack

---
+++ Heart Tracker/Heart Tracker/Heart_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heart_TrackerApp.swift
+//  Heart Tracker
+//
+//  Created by Israel Manzo on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Heart_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
