7610ec9900a5728bc7a328437abc08f3efe2ea86
Israel Manzo <israelmanzo814@gmail.com>
2025-02-03 10:50:03 -0500

    updated onboarding page

--- Heart Tracker/Heart Tracker/Onboarding/OnboardingPageView.swift
+++ Heart Tracker/Heart Tracker/Onboarding/OnboardingPageView.swift
@@ -7,24 +7,43 @@
 
 import SwiftUI
 
+struct PaginCount: Identifiable, Equatable {
+    let id = UUID()
+    var tag: Int
+    
+    static var previewPage = PaginCount(tag: 0)
+    
+    static var displayPages = [
+        PaginCount(tag: 0),
+        PaginCount(tag: 1),
+        PaginCount(tag: 2)
+    ]
+}
+
 struct Onboarding: Identifiable {
     var id: UUID = UUID()
     let title: String
     let description: String? = nil
+    let tag: Int
     
     static var listItems: [Onboarding] = [
-        Onboarding(title: "Welcome"),
-        Onboarding(title: "Seconde Page"),
-        Onboarding(title: "Third Page")
+        Onboarding(title: "Welcome", tag: 0),
+        Onboarding(title: "Seconde Page", tag: 1),
+        Onboarding(title: "Third Page", tag: 2)
     ]
 }
 
 struct OnboardingPageView: View {
     
+    @State private var selectedPage: Int = 0
+    private let pages = PaginCount.displayPages
+    
+    @ViewBuilder
     var body: some View {
-        TabView {
-            ForEach(Onboarding.listItems) { page in
-                OnboardingPage(page: page)
+        TabView(selection: $selectedPage) {
+            ForEach(pages) { page in
+                OnboardingPage(page: Onboarding.listItems[page.tag])
+                    .tag(page.tag)
             }
         }
         .tabViewStyle(.page(indexDisplayMode: .always))
