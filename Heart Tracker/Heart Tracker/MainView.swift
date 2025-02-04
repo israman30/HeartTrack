3c4663abfa43c56723bd2b4720861fcad419cf41
Israel Manzo <israelmanzo814@gmail.com>
2025-02-03 19:13:15 -0500

    Added Chart

--- Heart Tracker/Heart Tracker/MainView.swift
+++ Heart Tracker/Heart Tracker/MainView.swift
@@ -6,12 +6,36 @@
 //
 
 import SwiftUI
+import Charts
+import Observation
+
+struct Rate: Identifiable {
+    var id = UUID()
+    var rate: Double
+    var date: Date
+}
+
+@Observable
+class ChartViewModel {
+    var data: [Rate] = [
+        .init(rate: 115, date: Date().addingTimeInterval(90)),
+        .init(rate: 125, date: Date.now),
+        .init(rate: 112, date: Date.distantFuture),
+        .init(rate: 135, date: Date())
+    ]
+}
 
 struct MainView: View {
+    
+    var data = ChartViewModel()
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 Text("Hello, World!")
+                Chart(data.data) { data in
+                    LineMark(x: .value("Ratre", data.rate), y: .value("Date", data.date))
+                }
             }
             .navigationTitle("Heart Tracker")
         }
